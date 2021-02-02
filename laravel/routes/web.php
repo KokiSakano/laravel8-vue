82bcec373d3c9f59c267c16ac6a87f8ba2e0edc7
Koki Sakano <kosakanoki@gmail.com>
2021-02-02 14:00:08 +0900

    add login and register

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('register');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
