f95a939d681cc61013227fbf3baefb30479742ba
Koki Sakano <kosakanoki@gmail.com>
2021-03-05 11:41:16 +0900

    テスト用データベースの作成およびデータベースに関するテストの作成

---
+++ laravel/tests/Feature/DatabaseTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Whisper;
+
+class DatabaseTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testUserDatabase()
+    {
+        $testUser = new User();
+        $newdata = [
+            'name' => 'tester',
+            'email' => 'test@test.com',
+            'password' => 'testtest',
+        ];
+        $saveUser = $testUser -> fill($newdata) -> save();
+        $this->assertTrue($saveUser);
+    }
+    public function testWhisperDatabase()
+    {
+        $testWhisper = new Whisper();
+        $newdata = [
+            'whisp' => 'test',
+            'user_id' => 1,
+            'good' => 0,
+        ];
+        $saveWhisper = $testWhisper -> fill($newdata) -> save();
+        $this->assertTrue($saveWhisper);
+    }
+}
