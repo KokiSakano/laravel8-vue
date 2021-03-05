0ed2cbd6e7fe96aff9e4c4da5cba8872b255ea21
Koki Sakano <kosakanoki@gmail.com>
2021-03-05 17:33:47 +0900

    投稿に関する機能のテスト作成

---
+++ laravel/tests/Feature/WhisperTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use App\Models\User;
+use App\Models\Whisper;
+
+class WhisperTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+    /**
+     * 投稿テスト
+     */
+    public function testWhisperPost()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this->post('/api/whispers/', [
+            'whisper' => 'test'
+        ]);
+        $response -> assertStatus(200);
+        $this->assertEquals(1, Whisper::count());
+    }
+
+    /**
+     * 削除テスト
+     */
+    public function testWhisperDelete()
+    {
+        $user = User::factory()->create();
+        $whisper = Whisper::factory()->create();
+        $this->actingAs($user);
+
+        $route = '/api/whispers/' . strval($whisper->id);
+        $response = $this->delete($route);
+        $response -> assertStatus(200);
+        $this->assertEquals(0, Whisper::count());
+    }
+
+    /**
+     * 取得テスト
+     */
+    public function testWhisperGet()
+    {
+        $user = User::factory()->create();
+        $whisper = Whisper::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this->get('/api/whispers/');
+        $response -> assertStatus(200);
+    }
+}
