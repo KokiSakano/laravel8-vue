3639e9e577e0c21b9b6a27cf17d14cd2a7db3c1b
Koki Sakano <kosakanoki@gmail.com>
2021-03-17 13:59:44 +0900

    db設定

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -79,13 +79,18 @@ return [
         ],
 
         'testing' => [
-            'driver'   => 'sqlite',
-            'database' => ':memory:', // SQLiteのインメモリ機能を使用
-            'prefix'   => '',
-            'options'  => [
-                // テストデータの永続化
-                PDO::ATTR_PERSISTENT => false,
-            ],
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
